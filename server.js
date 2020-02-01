0ac38ed250f3eaef36e93ad7160918e38beac1d0
veh <verrell.soriano09@gmail.com>
2020-02-02 01:37:04 +0800

    server update

--- server.js
+++ server.js
@@ -21,4 +21,6 @@ app.use("/api/order", require("./routes/api/order"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(process.env.PORT || 5000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
