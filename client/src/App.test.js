bcdd97b4b7e32ffd661b8c7c0dce1ffb8d015a0a
veh <verrell.soriano09@gmail.com>
2020-02-02 13:35:52 +0800

    server updates

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,18 @@
+// import React from 'react';
+// import {shallow} from 'enzyme/build';
+// import App from './App';
+
+
+// it('mounts without crashing', () => {
+//   const wrapper = shallow(<App />);
+//   wrapper.unmount()
+// });
 import React from 'react';
-import {shallow} from 'enzyme/build';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-
-it('mounts without crashing', () => {
-  const wrapper = shallow(<App />);
-  wrapper.unmount()
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
