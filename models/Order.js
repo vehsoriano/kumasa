706d33d148ebdffe32b47f50e28c98765ec20141
dcjerald-fullstack <dcjerald@fullstack.ph>
2020-02-24 00:49:14 +0800

    Some update

--- models/Order.js
+++ models/Order.js
@@ -8,6 +8,15 @@ const OrderSchema = new Schema({
   order_id: {
     type: String
   },
+  rider_id: {
+    type: String
+  },
+  address: {
+    type: String
+  },
+  city: {
+    type: String
+  },
   status: {
     type: String,
     default: "Pending"
